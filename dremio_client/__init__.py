970f704936aa3cdd2f7356ad375dcc1448d48fb0
Ryan Murray <rymurr@dremio.com>
2019-08-13 10:35:24 +0100

    Bump version: 0.1.0 → 0.2.0

--- dremio_client/__init__.py
+++ dremio_client/__init__.py
@@ -6,7 +6,7 @@ from __future__ import division
 
 __author__ = """Ryan Murray"""
 __email__ = 'rymurr@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 import os
 from .dremio_client import DremioClient
