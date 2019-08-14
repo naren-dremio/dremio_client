b89ba5f1eac5fe3c3e59892b30e7f2bfb9196d45
Ryan Murray <rymurr@dremio.com>
2019-08-14 17:14:44 +0100

    Bump version: 0.2.4 → 0.3.0

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/rymurr/dremio_client',
-    version='0.2.4',
+    version='0.3.0',
     zip_safe=False,
     extras_require={
         ':python_version == "2.7"': ['futures']
