7d19fc2736152604beae1f54f9fc686f227a0ee4
Ryan Murray <rymurr@dremio.com>
2019-10-30 10:10:33 +0000

    fix auth

--- dremio_client/auth/__init__.py
+++ dremio_client/auth/__init__.py
@@ -25,6 +25,7 @@
 import os
 import json
 import time
+from confuse import NotFoundError, ConfigValueError
 from .basic import login as basic_auth  # NOQA
 from .config import login as config_auth
 
@@ -46,7 +47,10 @@ def _write_token(token, config_dict):
         if source.filename and not source.default:
             directory = os.path.dirname(source.filename)
             with open(os.path.join(directory, 'auth.json'), 'w') as f:
-                json.dump({'token': token, 'timestamp': time.time()}, f)
+                json.dump({'token': token,
+                           'timestamp': time.time(),
+                           'hostname': config_dict['hostname'].get(),
+                           'user': config_dict['auth']['username'].get()}, f)
             return
 
 
@@ -57,12 +61,23 @@ def _existing_token(config_dict):
             if os.path.exists(os.path.join(directory, 'auth.json')):
                 with open(os.path.join(directory, 'auth.json')) as f:
                     authfile = json.load(f)
-                    if _is_valid(authfile):
+                    if _is_valid(authfile, config_dict):
                         return authfile['token']
     raise KeyError
 
 
-def _is_valid(authfile):
+def _is_valid(authfile, config_dict):
+    hostname = authfile['hostname']
+    username = authfile['username']
+    try:
+        expected_hostname = config_dict['hostname'].get()
+        expected_username = config_dict['auth']['username'].get()
+    except (ConfigValueError, NotFoundError):
+        return False
+    if hostname != expected_hostname:
+        return False
+    if username != expected_username:
+        return False
     now = time.time()
     then = authfile['timestamp']
     return (then + 60 * 60 * 10) > now
